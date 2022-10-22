ce985ea4810817762a88c812078eaa3da095a33e
Jeferson Gomes <67127121+jeferson3@users.noreply.github.com>
2022-10-22 15:38:59 -0300

    configurações de tipos e banco de dados

--- app/config/database.js
+++ app/config/database.js
@@ -3,13 +3,15 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("urbanTaxi", "root", "", {
   host: "localhost",
   dialect: "mysql",
+  timezone: '-03:00',
+  logging: false
 });
 
 sequelize
   .authenticate()
   .then((r) => {})
   .catch((err) => {
-    console.error("Unable to connect to the database:", error);
+    console.error("Unable to connect to the database:", err);
   });
 
 module.exports = sequelize;
--- app/config/types.js
+++ app/config/types.js
@@ -1,5 +1,7 @@
-module.exports.BEM_VINDO = 'BEM_VINDO';
-module.exports.SOLICITADO = 'SOLICITADO';
-module.exports.ESCOLHER_TIPO = 'ESCOLHER_TIPO'
-module.exports.CONCLUIDO = 'CONCLUIDO'
-module.exports.CANCELADO = 'CANCELADO'
+module.exports.BEM_VINDO = 'BEM_VINDO'; // primeira mensagem
+module.exports.SOLICITADO = 'SOLICITADO'; // mensagem de nova solicitação
+module.exports.LOCALIZACAO_DE = 'LOCALIZACAO_DE'; // escolher localização de inicio de corrida
+module.exports.LOCALIZACAO_PARA = 'LOCALIZACAO_PARA'; // escolher localização de destino de corrida
+module.exports.ESCOLHER_TIPO = 'ESCOLHER_TIPO' // escolher tipo de transporte para a corrida
+module.exports.CONCLUIDO = 'CONCLUIDO' // concluir solicitação
+module.exports.CANCELADO = 'CANCELADO' // cancelar solicitação
