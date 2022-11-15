64bc9cd4b85cb7e8e87e80a3920bb661c19109a6
Jeferson Gomes <67127121+jeferson3@users.noreply.github.com>
2022-11-15 16:52:24 -0300

    att nome do banco de dados

--- app/config/database.js
+++ app/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("urbanTaxi", "root", "", {
+const sequelize = new Sequelize("taxiBot", "root", "", {
   host: "localhost",
   dialect: "mysql",
   timezone: '-03:00',
